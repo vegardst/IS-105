e9200f7d4a59d5e58c7cf42e86bc8adfdcbafe61
Erlend W <zw@zwirc.com>
2017-02-22 23:10:26 +0100

    Ascii_Test

--- ICA03/src/ascii/ascii_test.go
+++ ICA03/src/ascii/ascii_test.go
@@ -2,17 +2,15 @@ package ascii
 
 import 	"testing"
 
+
 func TestGreetingASCII(t *testing.T) {
 	ascii := GreetingASCII()
-	for i := 0; i < len(ascii); i++ {
-		if (isASCII(string(ascii[i])) == false){
-			t.Fail()
-			break
-		}
+	if !(isASCII(ascii)){
+		t.Fail()
 	}
-
 }
-// Fra https://play.golang.org/p/hnZzfnbXeF
+
+
 
 func isASCII(s string) bool {
 	for _, c := range s {
