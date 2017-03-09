1114f44ec31664f002eec3f21a6dd93e205eabc9
Erlend <zw@zwirc.com>
2017-03-09 17:02:13 +0100

    Formating - new fix soon

--- ICA03/src/main.go
+++ ICA03/src/main.go
@@ -195,10 +195,10 @@ func oppgave3c() {
 
 }
 func oppgave4a() {
-	const tekst = "\xE2\x80\x9C\x6E\x6F\x72\x64\x20\x6F\x67\x20\x73\xC3\xB8\x72\xE2\x80\x9D"
-	fmt.Println(unicode.Translate(tekst, "Norwegian"))
-	fmt.Println(unicode.Translate(tekst, "Icelandic"))
-	fmt.Println(unicode.Translate(tekst, "Japanese"))
+	const tekst = "\x48\x65\x6e\x72\x69\x6b\x20"
+	fmt.Printf(" Norwegian: %s \n", unicode.Translate(tekst, "Norwegian"))
+	fmt.Printf(" Icelandic: %s \n", unicode.Translate(tekst, "Icelandic"))
+	fmt.Printf(" Japanese: %s \n", unicode.Translate(tekst, "Japanese"))
 
 }
 func oppgave4b() {
